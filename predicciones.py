cba990aa6a91a0ebcf7a86cc6dac10ccb6622186
diferlan <a01751654@itesm.mx>
2023-09-11 20:06:41 -0600

    Actualizado predicciones: Falta regresar el json

--- predicciones.py
+++ predicciones.py
@@ -1,7 +1,14 @@
 import joblib
+import pandas as pd
 
-def predicciones(modelo='pipeline_houses_final.joblib',X):
-    media = X_[['YearBuilt', 'YearRemodAdd','GarageYrBlt','YrSold']].mean()
-    X[['YearBuilt', 'YearRemodAdd','GarageYrBlt','YrSold']].fillna(media)
+def predicciones(json,modelo='pipeline_houses_final.joblib'):
+    with open(json, 'r') as archivo:
+        casas_json = json.load(archivo)
+    # Convierte el JSON nuevamente en un DataFrame
+    df = pd.DataFrame(casas_json['casas'])  
+    df.drop(['Precio de venta'],axis=1,inplace=True)   
+    X=df.iloc[-1]
+    media = [['Año de construccion', 'Año de remodelacion','Año de construccion de garage','Año de venta']].mean()
+    X[['Año de construccion', 'Año de remodelacion','Año de construccion de garage','Año de venta']].fillna(media)
     X.fillna(0,inplace=True)
     return modelo.predict(X)
