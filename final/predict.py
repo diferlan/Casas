ff2028d108d37fe3dd49717f24ec835a7cbdec4e
diferlan <a01751654@itesm.mx>
2023-09-12 19:10:52 -0600

    py con función de predicción y modelo en joblib 2

---
+++ final/predict.py
@@ -0,0 +1,26 @@
+import joblib
+import pandas as pd
+import json
+
+def predicciones(json_file, modelo_path='/Users/diferlanderos/Desktop/RetoCasas/Casas/pipeline_houses_final_v3.joblib'):
+    modelo = joblib.load(modelo_path)
+    #print(modelo.feature_names_in_)
+    with open(json_file, 'r') as archivo:
+        casas_json = json.load(archivo)
+    # Convierte el JSON nuevamente en un DataFrame
+    df = pd.DataFrame(casas_json['casas'])
+    X = df.drop('SalePrice', axis=1)
+    #print(X.head())
+    # Calcula la media de las columnas específicas y rellena los valores faltantes en X
+    medias = df[['YearBuilt', 'YearRemodAdd','GarageYrBlt','YrSold']].mean()
+    X[['YearBuilt', 'YearRemodAdd','GarageYrBlt','YrSold']] = X[['YearBuilt', 'YearRemodAdd','GarageYrBlt','YrSold']].fillna(medias)
+    X.fillna(0, inplace=True)
+    predicciones=modelo.predict(X)
+    #print(predicciones[-1])
+    df.at[df.index[-1], 'SalePrice'] = predicciones[-1]
+    return df[['LotArea','Neighborhood','SalePrice']].tail(1).to_dict(orient='records')
+#Tamaño del lote', 'Colonia', 'Precio de venta'
+
+json_file = '/Users/diferlanderos/Desktop/RetoCasas/Casas/casas.json'
+resultado=predicciones(json_file)
+print(resultado)
