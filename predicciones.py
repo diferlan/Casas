ba0361598e9ed5fd5ffb8fe93070b5b279d99d11
diferlan <a01751654@itesm.mx>
2023-09-12 19:07:40 -0600

    py con función de predicción y modelo en joblib

--- predicciones.py
+++ predicciones.py
@@ -1,14 +1,37 @@
 import joblib
 import pandas as pd
+import json
 
-def predicciones(json,modelo='pipeline_houses_final.joblib'):
-    with open(json, 'r') as archivo:
+def predicciones(json_file, modelo_path='/Users/diferlanderos/Desktop/RetoCasas/Casas/pipeline_houses_final_v2.joblib'):
+    modelo = joblib.load(modelo_path)
+    print(modelo.feature_names_in_)
+    with open(json_file, 'r') as archivo:
         casas_json = json.load(archivo)
     # Convierte el JSON nuevamente en un DataFrame
     df = pd.DataFrame(casas_json['casas'])  
-    df.drop(['Precio de venta'],axis=1,inplace=True)   
-    X=df.iloc[-1]
-    media = [['Año de construccion', 'Año de remodelacion','Año de construccion de garage','Año de venta']].mean()
-    X[['Año de construccion', 'Año de remodelacion','Año de construccion de garage','Año de venta']].fillna(media)
-    X.fillna(0,inplace=True)
-    return modelo.predict(X)
+    # df.drop(['Precio de venta'], axis=1, inplace=True)
+    # print(df.columns)   
+    X = df.iloc[-1].copy()  # Copia la última fila
+    X = X.drop('Precio de venta') # Elimina la columna de precio de venta
+    X = X.to_frame() # Convierte X en un DataFrame de una columna
+    print(X.columns)    # Calcula la media de las columnas específicas y rellena los valores faltantes en X
+    medias = df[['Año de construccion', 'Año de remodelacion', 'Año de construccion de garage', 'Año de venta']].mean()
+    X[['Año de construccion', 'Año de remodelacion', 'Año de construccion de garage', 'Año de venta']] = X[['Año de construccion', 'Año de remodelacion', 'Año de construccion de garage', 'Año de venta']].fillna(medias)
+    X.fillna(0, inplace=True)
+    print(X)
+    # Realiza la transformación de las características utilizando el preprocesador
+    X_transformed = modelo.named_steps['preprocessor'].transform(X)
+    # Realiza la predicción
+    prediccion = modelo.predict(X_transformed)
+    # Agrega el resultado de la predicción a X
+    X['Precio de venta'] = prediccion
+    # Convierte X a un diccionario y genera el JSON de salida
+    resultado_json = X[['Tamaño del lote', 'Colonia', 'Precio de venta']].to_dict(orient='records')
+    # Guarda el JSON de resultado en un archivo
+    with open('prediccion.json', 'w') as archivo_salida:
+        json.dump(resultado_json, archivo_salida, indent=4)
+    return resultado_json
+
+json_file = '/Users/diferlanderos/Desktop/RetoCasas/Casas/casas.json'
+resultado = predicciones(json_file)
+print(resultado)
