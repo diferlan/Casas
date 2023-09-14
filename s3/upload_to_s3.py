7388268dcd4156624bde1b4b4ba6f47b34dd765f
diferlan <a01751654@itesm.mx>
2023-09-13 20:12:47 -0600

    Se agrega el código completo y ordenado

--- s3/upload_to_s3.py
+++ s3/upload_to_s3.py
@@ -2,11 +2,16 @@ import boto3
 import json
 import pandas as pd
 from datetime import datetime
+'''
+pwd
+ls -a
+nano ~/.aws/credentials
+cat ~/.aws/credentials
+'''
 def upload_to_s3(file):
     s3 = boto3.client('s3')
     timestamp = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-    s3.upload_file(Bucket = 'retocasas', Filename=rf'{file}',Key=f'/prueba/{timestamp}')
+    s3.upload_file(Bucket = 'retocasas', Filename=rf'{file}',Key=f'Krend/{timestamp}')
     return 'File uploaded to S3'
 
-upload_to_s3('/Users/diferlanderos/Desktop/RetoCasas/Casas/RetoCasasModelo.ipynb')
 
