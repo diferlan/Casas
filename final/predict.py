f8092d7101288dc8e2a24ecd6e090699970ee072
diferlan <a01751654@itesm.mx>
2023-09-16 15:59:32 -0600

    Tratamiento de datos para eliminar = y pasar a numericos los datos que deben ser numericos

--- final/predict.py
+++ final/predict.py
@@ -10,6 +10,22 @@ def predicciones(json_file, modelo_path='/Users/diferlanderos/Desktop/RetoCasas/
         casas_json = json.load(archivo)
     # Convierte el JSON nuevamente en un DataFrame
     df = pd.DataFrame(casas_json['casas'])
+    df = df.applymap(lambda x: x.replace('=', ''))
+    df[['id','MSSubClass', 'LotFrontage', 'LotArea', 'OverallQual', 'OverallCond',
+       'YearBuilt', 'YearRemodAdd', 'MasVnrArea', 'BsmtFinSF1', 'BsmtFinSF2',
+       'BsmtUnfSF', 'TotalBsmtSF', '1stFlrSF', '2ndFlrSF', 'LowQualFinSF',
+       'GrLivArea', 'BsmtFullBath', 'BsmtHalfBath', 'FullBath', 'HalfBath',
+       'BedroomAbvGr', 'KitchenAbvGr', 'TotRmsAbvGrd', 'Fireplaces',
+       'GarageYrBlt', 'GarageCars', 'GarageArea', 'WoodDeckSF', 'OpenPorchSF',
+       'EnclosedPorch', '3SsnPorch', 'ScreenPorch', 'PoolArea', 'MiscVal',
+       'MoSold', 'YrSold', 'SalePrice']] = pd.to_numeric(df[['id','MSSubClass', 'LotFrontage', 'LotArea', 'OverallQual', 'OverallCond',
+       'YearBuilt', 'YearRemodAdd', 'MasVnrArea', 'BsmtFinSF1', 'BsmtFinSF2',
+       'BsmtUnfSF', 'TotalBsmtSF', '1stFlrSF', '2ndFlrSF', 'LowQualFinSF',
+       'GrLivArea', 'BsmtFullBath', 'BsmtHalfBath', 'FullBath', 'HalfBath',
+       'BedroomAbvGr', 'KitchenAbvGr', 'TotRmsAbvGrd', 'Fireplaces',
+       'GarageYrBlt', 'GarageCars', 'GarageArea', 'WoodDeckSF', 'OpenPorchSF',
+       'EnclosedPorch', '3SsnPorch', 'ScreenPorch', 'PoolArea', 'MiscVal',
+       'MoSold', 'YrSold', 'SalePrice']])
     X = df.drop(['SalePrice','id'], axis=1)
     #print(X.head())
     # Calcula la media de las columnas específicas y rellena los valores faltantes en X
@@ -19,7 +35,7 @@ def predicciones(json_file, modelo_path='/Users/diferlanderos/Desktop/RetoCasas/
     predicciones=modelo.predict(X)
     #print(predicciones[-1])
     df.at[df.index[-1], 'SalePrice'] = predicciones[-1]
-    return df.tail(1).to_dict(orient='records')
+    return df.tail(1).to_json(orient='records')
 
 json_file = '/Users/diferlanderos/Desktop/RetoCasas/Casas/Codigo ordenado/KREND.json'
 resultado=predicciones(json_file)
